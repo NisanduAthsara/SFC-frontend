cd93a7c8efb59f029de23544d90d153a6f5fbbb0
NisanduAthsara <nisandunox@gmail.com>
2022-06-27 14:29:00 +0530

    Add logout feature

--- src/components/buyer/Profile/Profile.js
+++ src/components/buyer/Profile/Profile.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import axios from 'axios'
+import { useCookies } from "react-cookie";
 import ProfileUI from './ProfileUI'
 
 export default function Profile(){
     const [user,setUser] = React.useState({})
     const [token,setToken] = React.useState(null)
     const [userId,setUserId] = React.useState(null)
+    const [isDelOn,setIsDelOn] = React.useState(false)
+    const [isAccDel,setIsAccDel] = React.useState(false)
+    const [isLogOut,setIsLogOut] = React.useState(false)
+    const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
 
     let axiosConfig = {
         headers: {
@@ -72,11 +77,59 @@ export default function Profile(){
 
     },[userId])
 
+    React.useEffect(()=>{
+        if(token !== null){
+            const reqObj = {
+                token,
+                userId
+            }
+    
+            isAccDel === true && axios.delete('http://localhost:8080/deleteUser', reqObj, axiosConfig)
+                .then((res) => {
+                    if(res.data.success === false){
+                        // alert(res.data.message)
+                        // window.location.assign("/")
+                        console.log(res.data)
+                    }else{
+                        window.location.assign("/")
+                    }
+                })
+                .catch((err) => {
+                    console.log("AXIOS ERROR: ", err);
+                })
+        }
+    },[isAccDel])
+
+    React.useEffect(()=>{
+        if(isLogOut){
+            removeCookie('jwt',{path:'/'})
+            window.location.assign('/')
+        }
+    },[isLogOut])
+
+    function handleDelOn(){
+        setIsDelOn((prev)=>{
+            return !prev
+        })
+    }
+
+    function handleAccDel(){
+        setIsAccDel(true)
+    }
+
+    function handleLogout(){
+        setIsLogOut(true)
+    }
+
     return(
         <ProfileUI
             username={user.username}
             email={user.email}
             city={user.livingArea}
+            isDelOn={isDelOn}
+            handleDelOn={handleDelOn}
+            handleAccDel={handleAccDel}
+            handleLogout={handleLogout}
         />
     )
 }
--- src/components/buyer/Profile/ProfileUI.js
+++ src/components/buyer/Profile/ProfileUI.js
@@ -4,10 +4,19 @@ import {Link} from 'react-router-dom'
 export default function ProfileUI(props){
     return (
         <div>
-            <Link to="/buyer/profile/update">Update</Link>
-            <h1>{props.username}</h1>
-            <h3>{props.email}</h3>
-            <h3>{props.city}</h3>
+            {props.isDelOn && <div>
+                <div>Are you sure to delete</div>
+                <button onClick={props.handleAccDel}>Yes</button>  
+                <button onClick={props.handleDelOn}>Cancle</button>  
+            </div>}
+            <button onClick={props.handleLogout}>Logout</button>
+            <div>
+                <Link to="/buyer/profile/update">Update</Link>
+                <button onClick={props.handleDelOn}>Delete Your Account</button>
+                <h1>{props.username}</h1>
+                <h3>{props.email}</h3>
+                <h3>{props.city}</h3>
+            </div>
         </div>
     )
 }
