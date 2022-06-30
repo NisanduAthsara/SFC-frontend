d5bd9455cd7fb7f2a61782a4fb9a67915a1bd687
NisanduAthsara <nisandunox@gmail.com>
2022-06-30 22:45:18 +0530

    Make some changes

--- src/components/organization/Profile/Update/Update.js
+++ src/components/organization/Profile/Update/Update.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import UpdateUI from './UpdateUI'
 import {useCookies} from "react-cookie";
+import { getStorage, ref, deleteObject } from "firebase/storage";
+import {storage} from '../../signup/firebase/firebase'
+import { uploadBytes,getDownloadURL } from 'firebase/storage'
+import {v4} from 'uuid'
 import axios from 'axios'
 
 export default function Update(){
+    const [token,setToken] = React.useState(null)
     const [organization,setOrganization] = React.useState(null)
     const [orgToken,setOrgToken] = React.useState(null)
     const [orgId,setOrgId] = React.useState(null)
@@ -17,6 +22,9 @@ export default function Update(){
     const [openingHours,setOpeningHours] = React.useState(null)
     const [imgLink,setImgLink] = React.useState(null)
     const [userId,setUserId] = React.useState(null)
+    const [image,setImage] = React.useState(null)
+    const [isSubmitOn,setIsSubmitOn] = React.useState(false)
+    const [accountType,setAccountType] = React.useState(null)
 
     let axiosConfig = {
         headers: {
@@ -27,6 +35,8 @@ export default function Update(){
     React.useEffect(()=>{
         if(document.cookie.length > 1){
             const tokenCookie = cookies.orgJwt
+            const tokenUser = cookies.jwt
+            setToken(tokenUser)
             setOrgToken(tokenCookie)
         }
     },[0])
@@ -92,9 +102,158 @@ export default function Update(){
 
     },[orgId])
 
+    React.useEffect(()=>{
+        const reqObj = {
+            token,
+            id:userId
+        }
+
+        userId !== null &&axios.post('http://localhost:8080/getUser', reqObj, axiosConfig)
+			.then((res) => {
+                console.log(userId)
+				if(res.data.success === false){
+                    alert(res.data.message)
+                }else{
+                    setAccountType(res.data.user.accountType)
+                }
+			})
+			.catch((err) => {
+				console.log("AXIOS ERROR: ", err);
+			})
+    },[userId])
+
+    function handleRemoveImage(){
+        const storage = getStorage();
+        const desertRef = ref(storage, imgLink);
+
+        deleteObject(desertRef).then(() => {
+            alert('Successfully Deleted Image!')
+            setImgLink('')
+        }).catch((error) => {
+            alert('Something went wrong!')
+        });
+    }
+
+    function handleName(e){
+        setName(e.target.value)
+    }
+
+    function handleContact(e){
+        setContact(e.target.value)
+    }
+
+    function handleCity(e){
+        setCity(e.target.value)
+    }
+
+    function handleSellItem(e){
+        setSellItem(e.target.value)
+    }
+
+    function handleAddress(e){
+        setAddress(e.target.value)
+    }
+
+    function handleOpeningHours(e){
+        setOpeningHours(e.target.value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        // const storageFirebase = getStorage();
+        // const desertRef = ref(storageFirebase, imgLink);
+
+        if(imgLink.length > 1){
+            setImgLink(null)
+        }
+
+        //imgLink!==null && deleteObject(desertRef).then(() => {
+        if(image === null){
+            console.log(imgLink)
+            setIsSubmitOn(true)
+        }
+        // }).catch((error) => {
+        //     alert('Something went wrong!')
+        // });        
+    }
+
+    React.useEffect(()=>{
+        if(image === null) return
+        const imageTypes = ['image/jpeg','image/png','image/jpg']
+
+        if(!imageTypes.includes(image.type)){
+            alert('invalid file type')
+            return
+        }
+        const imageRef = ref(storage,`image/${image.name+v4()}`)
+        uploadBytes(imageRef,image).then((res)=>{
+            getDownloadURL(res.ref).then((response)=>{
+                setImgLink(response)
+                setIsSubmitOn(true)
+            })
+        })
+    },[image])
+
+    React.useEffect(()=>{
+        console.log(imgLink)
+        const reqObj = {
+            name,
+            contactNo:contact,
+            userId,
+            city,
+            sellItem,
+            address,
+            openingHours,
+            imgLink,
+            orgId,
+            token:orgToken,
+            accountType
+        }
+
+        isSubmitOn === true && axios.put('http://localhost:8080/updateOrg', reqObj, axiosConfig)
+            .then((res) => {
+                if(res.data.success === false){
+                    console.log(reqObj)
+                    console.log(res.data.message)
+                    // alert(res.data.message)
+                }else{
+                    console.log('done')
+                    alert(res.data.message)
+                }
+            })
+            .catch((err) => {
+                console.log("AXIOS ERROR: ", err);
+            })
+    },[isSubmitOn])
+
+    function handleChangeSubmit(e){
+        e.preventDefault()
+    }
+
+    function handleImage(e){
+        setImage(e.target.files[0])
+    }
+
     return(
         <UpdateUI
-            organization={organization}
+            name={name}
+            contact={contact}
+            city={city}
+            sellItem={sellItem}
+            address={address}
+            openingHours={openingHours}
+            imgLink={imgLink}
+            handleName={handleName}
+            handleContact={handleContact}
+            handleCity={handleCity}
+            handleSellItem={handleSellItem}
+            handleAddress={handleAddress}
+            handleOpeningHours={handleOpeningHours}
+            handleSubmit={handleSubmit}
+            handleRemoveImage={handleRemoveImage}
+            handleChangeSubmit={handleChangeSubmit}
+            handleImage={handleImage}
         />
     )
 }
--- src/components/organization/Profile/Update/UpdateUI.js
+++ src/components/organization/Profile/Update/UpdateUI.js
@@ -1,12 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function UpdateUI(props){
-    const {organization} = props
+    const {name,contact,city,sellItem,address,openingHours,imgLink} = props
+    const [isChange,setIsChange] = useState(false)
+
+    function handleChange(){
+        setIsChange((prev)=>{
+            return !prev
+        })
+    }
+
     return(
         <div>
-            {organization !== null && 
+            {imgLink !== null && 
                 <div>
-                    <h2>{organization.name}</h2>
+                    <form>
+                        <input type="text" onChange={(e)=>props.handleName(e)} value={name}/>
+                        <input type="text" onChange={(e)=>props.handleContact(e)} value={contact}/>
+                        <select onChange={(e)=>props.handleCity(e)} value={city}>
+                            <option selected>--Select City--</option>
+                            <option value="Kalutara">Kalutara</option>
+                            <option value="Colombo">Colombo</option>
+                            <option value="Gampaha">Gampaha</option>
+                            <option value="Ampara">Ampara</option>
+                            <option value="Anuradhapura">Anuradhapura</option>
+                            <option value="Badulla">Badulla</option>
+                            <option value="Batticaloa">Batticaloa</option>
+                            <option value="Galle">Galle</option>
+                            <option value="Hambantota">Hambantota</option>
+                            <option value="Jaffna">Jaffna</option>
+                            <option value="Kandy">Kandy</option>
+                            <option value="Kegalla">Kegalla</option>
+                            <option value="Kilinochchi">Kilinochchi</option>
+                            <option value="Kurunegala">Kurunegala</option>
+                            <option value="Mannar">Mannar</option>
+                            <option value="Matale">Matale</option>
+                            <option value="Matara">Matara</option>
+                            <option value="Moneragala">Moneragala</option>
+                            <option value="Mullaitivu">Mullaitivu</option>
+                            <option value="Nuwara Eliya">Nuwara Eliya</option>
+                            <option value="Polonnaruwa">Polonnaruwa</option>
+                            <option value="Puttalam">Puttalam</option>
+                            <option value="Ratnapura">Ratnapura</option>
+                            <option value="Trincomalee">Trincomalee</option>
+                            <option value="Vavuniya">Vavuniya</option>
+                        </select>
+                        <select onChange={(e)=>props.handleSellItem(e)} value={sellItem}>
+                            <option>--Select Selling Item Type--</option>
+                            <option value="fuel">Fuel</option>
+                            <option value="gas">Gas</option>
+                            <option value="milk powder">Milk Powder</option>
+                        </select>
+                        <input type="text" onChange={(e)=>props.handleAddress(e)} value={address}/>
+                        <input type="text" onChange={(e)=>props.handleOpeningHours(e)} value={openingHours}/>
+                        <button onClick={(e)=>props.handleSubmit(e)}>Sign Up</button>
+                    </form>
+                    <div>
+                        <img src={imgLink} width="80" height="80"/>
+                        {imgLink !== null && <button onClick={props.handleRemoveImage}>Remove Image</button>}
+                        {!isChange && <button onClick={handleChange}>Change Image</button>}
+                        {isChange && <div>
+                            <input type="file" onChange={props.handleImage}/>
+                            <button onClick={handleChange}>Cancel</button>
+                            <input type="submit" onClick={props.handleChangeSubmit}/>
+                        </div>}
+                    </div>
                 </div>
             }
         </div>
